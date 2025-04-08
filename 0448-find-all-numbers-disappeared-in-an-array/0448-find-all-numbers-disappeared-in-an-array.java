3f6f29477df91b0696324ce30985d6f05c504eeb
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2025-04-08 18:27:03 +0530

    Time: 6 ms (83.09%), Space: 56.6 MB (63.05%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,17 +1,36 @@
 class Solution {
-        public List<Integer> findDisappearedNumbers(int[] nums) {
-        for (int i : nums) {
-            int index = Math.abs(i);
-            if (nums[index - 1] > 0) {
-                nums[index - 1] *= -1;
+
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        int n=nums.length;
+        int index =0;
+        
+        //sorting the array using cyclic sort such that repated 
+        //numbers take place in place of missing numbers index
+
+        while(index<n){
+            int element = nums[index];
+            int correct = element-1;  // because array is from [1,N]
+            if(nums[index]!=nums[correct]){
+                int temp=nums[index];
+                nums[index]=nums[correct];
+                nums[correct]=temp;
+            }
+            else{
+                index++;
             }
         }
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                res.add(i + 1);
+
+        List<Integer> ls = new ArrayList<>();
+
+        //traversing the array for elements and its corresponding index not matching
+        //here we comparing with i+1 because array is from [1,N]
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                ls.add(i+1);
             }
         }
-        return res;
+        return ls;
     }
 }
