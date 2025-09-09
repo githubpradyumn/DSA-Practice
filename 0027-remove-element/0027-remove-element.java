91d4bf4159a4188160d77eac1a6aad92d7b66cbe
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2025-09-10 00:31:05 +0530

    Time: 0 ms (100%), Space: 42 MB (48.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,15 +1,13 @@
 class Solution {
      public static int removeElement(int[] nums, int val) {
-        int pnt = 0;
-        if (nums.length==0){
-            return 0;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[pnt] = nums[i];
-                pnt++;
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if ( nums[i] != val ){
+                nums[count] = nums[i];
+                count++;
             }
         }
-        return pnt;
+        return count;
     }
 }
