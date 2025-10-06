01c1172c25240dfbcc7b9fa989be1c4c14f29fea
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2025-10-07 01:05:35 +0530

    Time: 1 ms (94.27%), Space: 45.6 MB (54.82%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -1,31 +1,50 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         ListNode curr = head;
+//         ListNode prev = new ListNode(-1);
+//         ListNode start = prev;
+//         while(curr!=null){
+//             while(curr!=null && curr.val==val){
+//                 curr = curr.next;
+//             }
+//             while(curr!=null && curr.val!=val){
+//                 prev.next = curr;
+//                 curr = curr.next;
+//                 prev = prev.next;
+//             }
+//         }
+//         if(curr==null){
+//             prev.next = null;
+//         }
+//         return start.next;
+//     }
+// }
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
         ListNode curr = head;
-        ListNode prev = new ListNode(-1);
-        ListNode start = prev;
-        while(curr!=null){
-            while(curr!=null && curr.val==val){
-                curr = curr.next;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;  // unlink node
+            } else {
+                prev = curr;  // move prev only if node is kept
             }
-            while(curr!=null && curr.val!=val){
-                prev.next = curr;
-                curr = curr.next;
-                prev = prev.next;
-            }
+            curr = curr.next;
         }
-        if(curr==null){
-            prev.next = null;
-        }
-        return start.next;
+
+        return dummy.next;
     }
 }
