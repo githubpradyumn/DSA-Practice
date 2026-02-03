bcdf00a4e206014261a97f480d638e0ffa59aa22
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-04 02:27:04 +0530

    Time: 2 ms (94.29%), Space: 72.1 MB (38.9%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] pre = new int[n];
-        int[] suf = new int[n];
-        pre[0]=nums[0];
-        suf[n-1]=nums[n-1];
-        for(int i=1,j=n-2;i<n&&j>=0;i++,j--){
-            pre[i]=pre[i-1]*nums[i];
-            suf[j]=suf[j+1]*nums[j];
-        }
-        nums[0]=suf[1];
-        nums[n-1]=pre[n-2];
-        for(int i=1;i<n-1;i++){
-            nums[i]=pre[i-1]*suf[i+1];
-        }
-        return nums;
+        int[] ans = new int[n];
+        int total_product = 1;
+        int count = 0;
+        int index = 0;
 
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                count++;
+                index=i;
+            } else {
+                total_product *= nums[i];
+            }
+        }
+        if(count>1){
+            return ans;
+        } else if(count==1){
+            ans[index]=total_product;
+        } else {
+            for(int i=0; i<n; i++){
+                ans[i]=total_product/nums[i];
+            }
+        }
+        return ans;
     }
 }
