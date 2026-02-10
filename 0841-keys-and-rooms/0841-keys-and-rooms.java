ac49a12544fb91945f49d6e45e36101fe9e3d94b
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-10 11:58:43 +0530

    Time: 1 ms (85.03%), Space: 46.5 MB (26.49%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,22 +1,32 @@
 class Solution {
-    public void bfs(List<List<Integer>> graph, boolean[] isVisited){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        isVisited[0] = true;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            for(int ngbr : graph.get(curr)){
-                if(!isVisited[ngbr]){
-                    q.add(ngbr);
-                    isVisited[ngbr] = true;
-                }
+    // public void bfs(List<List<Integer>> graph, boolean[] isVisited){
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(0);
+    //     isVisited[0] = true;
+    //     while(!q.isEmpty()){
+    //         int curr = q.poll();
+    //         for(int ngbr : graph.get(curr)){
+    //             if(!isVisited[ngbr]){
+    //                 q.add(ngbr);
+    //                 isVisited[ngbr] = true;
+    //             }
+    //         }
+    //     }
+    // }
+    public void dfs(List<List<Integer>> graph, boolean[] isVisited,int start){
+        isVisited[start]=true;
+        for(int nbr:graph.get(start)){
+            if(!isVisited[nbr]){
+                dfs(graph,isVisited,nbr);
             }
         }
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] isVisited = new boolean[n];
-        bfs(rooms, isVisited);
+        int start = 0;
+        // bfs(rooms, isVisited);
+        dfs(rooms,isVisited,start);
 
         for(boolean unlocked : isVisited){
             if(!unlocked) return false;
