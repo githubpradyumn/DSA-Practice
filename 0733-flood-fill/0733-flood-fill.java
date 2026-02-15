b4417c7be120b8f90dcb077d5b44b82f28122a68
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-15 18:50:09 +0530

    Time: 2 ms (13.04%), Space: 47 MB (43.15%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,64 +1,55 @@
 class Solution {
     class Pair{
-        int row;
-        int col;
-
-        Pair(int row,int col){
+        int row , col;
+        Pair(int row, int col){
             this.row=row;
             this.col=col;
         }
     }
-    int[] rowDirection = {1,-1,0,0};
-    int[] colDirection = {0,0,1,-1};
 
-    public boolean isValid(int row,int col,int totalRows,int totalCols){
-        if(row>=0 && row<totalRows && col>=0 && col<totalCols) return true;
-        return false;
+    int rowDirection[] = {0,0,-1,1}; 
+    int colDirection[] = {-1,1,0,0}; 
+    
+    public boolean isValid(int row, int col, int totalRows, int totalCols){
+        return (row>=0 && col>=0 && row<totalRows && col<totalCols);
     }
 
-    public int[][] bfs(int[][] image,int[][] colored,boolean[][] visited ,int sr, int sc, int color,int colour,int m,int n){
-
+    public int[][] bfs(int[][] image,int[][] ans, int[][] isVisited,int color,int orgColor,int row,int col, int totalRows, int totalCols){
         Queue<Pair> q = new LinkedList<>();
-        
-        Pair p = new Pair(sr,sc);
-        q.add(p);
-        
-        colored[sr][sc]=color;
-        visited[sr][sc]=true;
-        
-        while(!q.isEmpty()){    
-
+        q.add(new Pair(row,col));
+        isVisited[row][col]=1;
+        ans[row][col]=color;
+        while(!q.isEmpty()){
             Pair curr = q.poll();
-            int currPairRow = curr.row;
-            int currPairCol = curr.col;
+            for(int i=0;i<4;i++){
+                int newRow = curr.row + rowDirection[i];
+                int newCol = curr.col + colDirection[i];
+                if(isValid(newRow,newCol,totalRows,totalCols) && image[newRow][newCol]==orgColor && isVisited[newRow][newCol]==0){
+                    q.add(new Pair(newRow,newCol));
+                    isVisited[newRow][newCol]=1;
+                    ans[newRow][newCol]=color;
 
-            for(int i=0; i<4; i++){
-                int newRow = currPairRow + rowDirection[i];
-                int newCol = currPairCol + colDirection[i];  
-                if(isValid(newRow,newCol,m,n) && image[newRow][newCol]==colour && visited[newRow][newCol]==false){
-                    Pair newp = new Pair(newRow,newCol);
-                    q.add(newp);
-                    visited[newRow][newCol]=true;
-                    colored[newRow][newCol]=color;
-                }  
+                }
             }
-
         }
-        return colored;
+        return ans;
     }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        int m = image.length;
-        int n = image[0].length;
 
-        int[][] colored = image;
-        boolean[][] visited = new boolean[m][n];
-        int colour = image[sr][sc];
-        for(int i=sr;i<m;i++){
-            for(int j=sc;j<m;j++){
-                bfs(image,colored,visited,sr,sc,color,colour,m,n);
-            }
-        }
-        return colored;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        int totalRows = image.length;
+        int totalCols = image[0].length;
+        int[][] isVisited = new int[totalRows][totalCols];
+        int[][] ans = image;
+        int orgColor = image[sr][sc];
+        
+        // for(int i=sr;i<totalRows;i++){
+            // for(int j=sc;j<totalCols;j++){
+                // if(image[i][j]==orgColor && isVisited[i][j]==0){
+                    bfs(image,ans,isVisited,color,orgColor,sr,sc,totalRows,totalCols);
+                // }
+            // }
+        // }
+        return ans;
     }
 }
