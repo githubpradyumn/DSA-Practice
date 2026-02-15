6115cd5407f9d874389df28891648aaabf17b822
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-15 21:07:33 +0530

    Time: 7 ms (85.48%), Space: 57.4 MB (69.45%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,83 +1,70 @@
 class Solution {
     class Pair{
-
-        int row;
-        int col;        
-
-        Pair(int row,int col){
+        int row , col;
+        Pair(int row, int col){
             this.row=row;
             this.col=col;
         }
-
-    }
-    public boolean isValid(int row,int col,int totalRows,int totalCols){
-        if(row>=0 && row<totalRows && col>=0 && col<totalCols) return true;
-        return false;
     }
 
-    int[] rowDirection = {1,-1,0,0};
-    int[] colDirection = {0,0,1,-1};
+    int rowDirection[] = {0,0,-1,1}; 
+    int colDirection[] = {-1,1,0,0}; 
+    
+    public boolean isValid(int row, int col, int totalRows, int totalCols){
+        return (row>=0 && col>=0 && row<totalRows && col<totalCols);
+    }    
 
-    public void bfs(boolean[][] isVisited,int[][] grid,int row,int col,int totalRows, int totalCols){
-        
+    public void bfs(int[][] grid, int[][] isVisited,int row,int col, int totalRows, int totalCols){
         Queue<Pair> q = new LinkedList<>();
-        Pair p = new Pair(row,col);
-        q.add(p);
-        isVisited[row][col] = true;
+        q.add(new Pair(row,col));
+        isVisited[row][col]=1;
         grid[row][col]=0;
-        
         while(!q.isEmpty()){
             Pair curr = q.poll();
-            
-            int currRow = curr.row;
-            int currCol = curr.col;;
-            
             for(int i=0;i<4;i++){
-                
-                int newRow = currRow + rowDirection[i];
-                int newCol = currCol + colDirection[i];
-                
-                if(isValid(newRow,newCol,totalRows,totalCols) && grid[newRow][newCol]==1 && isVisited[newRow][newCol]==false){
-                    Pair currPair = new Pair(newRow,newCol);
-                    q.add(currPair);
-                    isVisited[newRow][newCol]=true;
+                int newRow = curr.row + rowDirection[i];
+                int newCol = curr.col + colDirection[i];
+                if(isValid(newRow,newCol,totalRows,totalCols) && grid[newRow][newCol]==1 && isVisited[newRow][newCol]==0){
+                    q.add(new Pair(newRow,newCol));
+                    isVisited[newRow][newCol]=1;
                     grid[newRow][newCol]=0;
                 }
-                
             }
         }
     }
     public int numEnclaves(int[][] grid){
-        
-        int totalRows=grid.length;
-        int totalCols=grid[0].length;
-        boolean[][] isVisited = new boolean[totalRows][totalCols];
+
+        int totalRows = grid.length;
+        int totalCols = grid[0].length;
+        int[][] isVisited = new int[totalRows][totalCols];
         int count = 0;
+
         for(int i=0;i<totalCols;i++){
-            if(grid[0][i]==1 && isVisited[0][i]==false){
-                bfs(isVisited,grid,0,i,totalRows,totalCols);
+            if(grid[0][i]==1 && isVisited[0][i]==0){
+                bfs(grid,isVisited,0,i,totalRows,totalCols);
             }
-            if(grid[totalRows-1][i]==1 && isVisited[totalRows-1][i]==false){
-                bfs(isVisited,grid,totalRows-1,i,totalRows,totalCols);
-            }
-        }
-        for(int i=0;i<totalRows;i++){
-            if(grid[i][0]==1 && isVisited[i][0]==false){
-                bfs(isVisited,grid,i,0,totalRows,totalCols);
-            }
-            if(grid[i][totalCols-1]==1 && isVisited[i][totalCols-1]==false){
-                bfs(isVisited,grid,i,totalCols-1,totalRows,totalCols);
+            if(grid[totalRows-1][i]==1 && isVisited[totalRows-1][i]==0){
+                bfs(grid,isVisited,totalRows-1,i,totalRows,totalCols);
             }
         }
+
         for(int i=0;i<totalRows;i++){
-            for(int j=0;j<totalCols;j++){
+            if(grid[i][0]==1 && isVisited[i][0]==0){
+                bfs(grid,isVisited,i,0,totalRows,totalCols);
+            }
+            if(grid[i][totalCols-1]==1 && isVisited[i][totalCols-1]==0){
+                bfs(grid,isVisited,i,totalCols-1,totalRows,totalCols);
+            }
+        }
+
+        for(int i=1;i<totalRows-1;i++){
+            for(int j=1;j<totalCols-1;j++){
                 if(grid[i][j]==1){
-                    // bfs(isVisited,grid,i,j,totalRows,totalCols);
                     count++;
                 }
             }
-
-        }
+        } 
         return count;
-    }
+    }    
 }
+
