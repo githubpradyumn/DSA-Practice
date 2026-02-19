ca1ecb6b1d07681f104c4f5ebee1dd2f4cd0ba11
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-20 00:34:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isBipartite(int V, int[][] edges) {
+        // Code here
+         ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
+        for(int i=0;i<V;i++) adjlist.add(new ArrayList<>());
+        for(int[] e : edges){
+          adjlist.get(e[0]).add(e[1]);
+        }
+        int[] color = new int[V];
+        for(int i=0;i<V;i++)
+            color[i] = -1;
+        for(int i=0;i<V;i++){
+            if(color[i] == -1){
+                if(!bfsCheck(i,adjlist,color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean bfsCheck(int start, ArrayList<ArrayList<Integer>> adj, int[] color){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        color[start] = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int nb : adj.get(node)){
+                if(color[nb] == -1){
+                    color[nb] = 1 - color[node];
+                    q.add(nb);
+                }
+                else if(color[nb] == color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
