e8f14a57a8c9482535a4cfb34edf31e1c9116acc
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-19 16:08:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adjlist.add(new ArrayList<>());
+        }
+        for (int[] e : edges) {
+            adjlist.get(e[0]).add(e[1]);
+            // adj.get(e[1]).add(e[0]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int[] inDegree = new int[V];
+        for(int i=0;i<V;i++){
+            for(int nbr: adjlist.get(i)){
+                inDegree[nbr]++;
+            }
+        }
+        for(int i=0;i<V;i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            ans.add(curr);
+            for(int nbr:adjlist.get(curr)){
+                inDegree[nbr]--;
+                if(inDegree[nbr]==0){
+                    q.add(nbr);
+                }
+            }
+        }
+        return ans;
+    }
+}
