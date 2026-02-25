4e297edc05698a3cc62a9588a3a7e7c2756a6a3e
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-02-25 12:07:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,36 +1,37 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-        ArrayList<Integer> ans = new ArrayList<>();
         ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
-        for(int i=0;i<V;i++) adjlist.add(new ArrayList<>());
-        
+        for(int i=0; i<V; i++){
+            adjlist.add(new ArrayList<>());
+        }
         for(int[] e : edges){
             adjlist.get(e[0]).add(e[1]);
         }
-        
-        Queue<Integer> q = new LinkedList<>();
-        int[] degree = new int[V];
-        for(int i=0;i<V;i++){
-            for(int nbr:adjlist.get(i)){
-                degree[nbr]++;
+        int[] indegree = new int[V];
+        for(int i=0; i<V; i++){
+            for(int nbh : adjlist.get(i)){
+                indegree[nbh]++;
             }
         }
-        for(int i=0;i<V;i++){
-            if(degree[i]==0){
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0){
                 q.add(i);
             }
         }
+        ArrayList<Integer> ansList = new ArrayList<>();
         while(!q.isEmpty()){
             int curr = q.poll();
-            ans.add(curr);
-            for(int nbr:adjlist.get(curr)){
-                degree[nbr]--;
-                if(degree[nbr]==0){
-                    q.add(nbr);
+            ansList.add(curr);
+            for(int nbh: adjlist.get(curr)){
+                indegree[nbh]--;
+                if(indegree[nbh]==0){
+                    q.add(nbh);
                 }
             }
+                
         }
-        return ans;
+        return ansList;
     }
 }
