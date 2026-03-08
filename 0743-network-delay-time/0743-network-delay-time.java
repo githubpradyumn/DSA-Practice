efa8e33aec3ec311f4ba6e300feabef043900615
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-08 23:31:18 +0530

    Time: 12 ms (59.42%), Space: 49 MB (72.85%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,67 +1,31 @@
 class Solution {
-    class Pair {
-        int node;
-        int dist;
-
-        Pair(int node, int dist){
-            this.node = node;
-            this.dist = dist;
-        }
-    }
-
+    
     public int networkDelayTime(int[][] times, int V, int src) {
-        ArrayList<ArrayList<Pair>> adjlist = new ArrayList<>();
 
-        for(int i=0;i<V;i++){
-            adjlist.add(new ArrayList<>());
-        }
+        int[] dist = new int[V+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
-        for(int i=0;i<times.length;i++){
+        for(int i = 1; i <= V-1; i++){
+            for(int[] e : times){
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
 
-            int u = times[i][0]-1;
-            int v = times[i][1]-1;
-            int w = times[i][2];
-
-            adjlist.get(u).add(new Pair(v,w));
-            // adjlist.get(v).add(new Pair(u,w));
-        }
-        int[] dist = new int[V];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-    //Priority Queue implementation
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a,b) -> a.dist - b.dist
-        );
-
-        dist[src-1] = 0;
-        pq.add(new Pair(src-1,0));
-
-        
-        while(!pq.isEmpty()){
-
-            Pair curr = pq.poll();
-            int node = curr.node;
-            int currDist = curr.dist;
-
-            for(Pair neighbour : adjlist.get(node)){
-
-                int adjNode = neighbour.node;
-                int weight = neighbour.dist;
-
-                if(currDist + weight < dist[adjNode]){
-                    dist[adjNode] = currDist + weight;
-                    pq.add(new Pair(adjNode, dist[adjNode]));
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
                 }
             }
         }
-        int time = 0;
-        for(int i=0;i<dist.length;i++){
-            if(dist[i]==Integer.MAX_VALUE){
+
+        int maxTime = 0;
+
+        for(int i = 1; i <= V; i++){
+            if(dist[i] == Integer.MAX_VALUE)
                 return -1;
-            } else {
-                time = Math.max(time,dist[i]);
-            }
+            maxTime = Math.max(maxTime, dist[i]);
         }
-        return time;
+
+        return maxTime;
     }
 }
