aaa2f8c921bef27fbf36743744bedaf0c30e387f
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-11 12:19:07 +0530

    Time: 548 ms (91.45%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,9 @@
+WITH DuplicateEmails AS (
+    SELECT id,ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num FROM Person
+)
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM DuplicateEmails
+    WHERE row_num > 1
+);
