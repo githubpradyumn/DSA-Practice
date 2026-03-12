ee43234f05112b3059ae9acfdaf9a9a2757b60d3
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-12 10:27:54 +0530

    Time: 17 ms (6.18%), Space: 53.2 MB (94.51%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,26 +1,73 @@
+import java.util.*;
+
 class Solution {
+
+    class Pair {
+        int r;
+        int c;
+
+        Pair(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
     public int countServers(int[][] grid) {
-        
+
         int m = grid.length;
         int n = grid[0].length;
 
-        int[] row = new int[m];
-        int[] col = new int[n];
+        boolean[][] visited = new boolean[m][n];
+
+        int result = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    row[i]++;
-                    col[j]++;
+
+                if(grid[i][j] == 1 && !visited[i][j]){
+
+                    int size = bfs(grid, visited, i, j);
+
+                    if(size > 1){
+                        result += size;
+                    }
                 }
             }
         }
 
-        int count = 0;
+        return result;
+    }
 
-        for(int i = 0; i < m; i++){
+    private int bfs(int[][] grid, boolean[][] visited, int r, int c){
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(r, c));
+        visited[r][c] = true;
+
+        int count = 1;
+
+        while(!q.isEmpty()){
+
+            Pair cur = q.poll();
+            int x = cur.r;
+            int y = cur.c;
+
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)){
+                if(grid[x][j] == 1 && !visited[x][j]){
+                    visited[x][j] = true;
+                    q.offer(new Pair(x, j));
+                    count++;
+                }
+            }
+
+            for(int i = 0; i < m; i++){
+                if(grid[i][y] == 1 && !visited[i][y]){
+                    visited[i][y] = true;
+                    q.offer(new Pair(i, y));
                     count++;
                 }
             }
@@ -29,3 +76,4 @@ class Solution {
         return count;
     }
 }
+
