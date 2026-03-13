babef40e78c83d73ee787c31687acfb2f40976f9
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-13 11:57:48 +0530

    Time: 1 ms (57.6%), Space: 42.9 MB (92.04%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int splitArray(int[] weights, int days) {
+        int low = 0;
+        int high = 0;
+
+        for (int w : weights) {
+            low = Math.max(low, w);
+            high += w;
+        }
+
+        int ans = high;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (isValid(weights, days, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean isValid(int[] weights, int days, int capacity) {
+
+        int dayCount = 1;
+        int load = 0;
+
+        for (int w : weights) {
+
+            if (load + w > capacity) {
+                dayCount++;
+                load = w;
+            } else {
+                load += w;
+            }
+        }
+
+        return dayCount <= days;
+    }
+}
