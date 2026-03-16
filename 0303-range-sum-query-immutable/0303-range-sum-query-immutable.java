8032013ad93ff11ad269e9f7141cf6f0fc9061f6
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-16 15:09:40 +0530

    Time: 10 ms (20.08%), Space: 51.1 MB (5.77%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,20 +1,55 @@
 class NumArray {
-    int[] arr;
-    public NumArray(int[] nums) {
-        arr=nums;
-    }
-    
-    public int sumRange(int left, int right) {
-        int sum=0;
-        for(int i = left;i<=right;i++){
-            sum += arr[i];
+
+    SegmentTree st;
+
+    class SegmentTree {
+
+        int[] tree;
+        int n;
+
+        SegmentTree(int[] arr) {
+            n = arr.length;
+            tree = new int[4 * n];
+            build(arr, 1, 0, n - 1);
         }
-        return sum;
+
+        void build(int[] arr, int node, int start, int end) {
+
+            if (start == end) {
+                tree[node] = arr[start];
+                return;
+            }
+
+            int mid = start + (end - start) / 2;
+
+            build(arr, 2 * node, start, mid);
+            build(arr, 2 * node + 1, mid + 1, end);
+
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+
+        int query(int node, int start, int end, int l, int r) {
+
+            if (r < start || end < l)
+                return 0;
+
+            if (l <= start && end <= r)
+                return tree[node];
+
+            int mid = start + (end - start) / 2;
+
+            int left = query(2 * node, start, mid, l, r);
+            int right = query(2 * node + 1, mid + 1, end, l, r);
+
+            return left + right;
+        }
+    }
+
+    public NumArray(int[] nums) {
+        st = new SegmentTree(nums);
+    }
+
+    public int sumRange(int left, int right) {
+        return st.query(1, 0, st.n - 1, left, right);
     }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(left,right);
- */
