e0af5fe4adc06b348d0048c24739ea059693765e
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-19 12:32:58 +0530

    Time: 3 ms (16.05%), Space: 46.5 MB (63.9%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,47 +1,47 @@
 class Solution {
-    // class SegmentTree {
-    //     int[] tree;
-    //     int n;
+    class SegmentTree {
+        int[] tree;
+        int n;
 
-    //     SegmentTree(int[] arr) {
-    //         n = arr.length;
-    //         tree = new int[4 * n];
-    //         build(arr, 1, 0, n - 1);
-    //     }
+        SegmentTree(int[] arr) {
+            n = arr.length;
+            tree = new int[4 * n];
+            build(arr, 1, 0, n - 1);
+        }
 
-    //     void build(int[] arr, int node, int start, int end) {
+        void build(int[] arr, int node, int start, int end) {
 
-    //         if (start == end) {
-    //             tree[node] = arr[start];
-    //             return;
-    //         }
+            if (start == end) {
+                tree[node] = arr[start];
+                return;
+            }
 
-    //         int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
 
-    //         build(arr, 2 * node, start, mid);
-    //         build(arr, 2 * node + 1, mid + 1, end);
+            build(arr, 2 * node, start, mid);
+            build(arr, 2 * node + 1, mid + 1, end);
 
-    //         tree[node] = tree[2 * node] + tree[2 * node + 1];
-    //     }
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
 
-    //     int query(int node, int start, int end, int l, int r) {
+        int query(int node, int start, int end, int l, int r) {
 
-    //         // No overlap
-    //         if (r < start || end < l)
-    //             return 0;
+            // No overlap
+            if (r < start || end < l)
+                return 0;
 
-    //         // Complete overlap
-    //         if (l <= start && end <= r)
-    //             return tree[node];
+            // Complete overlap
+            if (l <= start && end <= r)
+                return tree[node];
 
-    //         int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
 
-    //         int left = query(2 * node, start, mid, l, r);
-    //         int right = query(2 * node + 1, mid + 1, end, l, r);
+            int left = query(2 * node, start, mid, l, r);
+            int right = query(2 * node + 1, mid + 1, end, l, r);
 
-    //         return left + right;
-    //     }
-    // }
+            return left + right;
+        }
+    }
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         int n = fruits.length;
         int[] arr = new int[n];
@@ -57,15 +57,9 @@ class Solution {
                 idx++;
             }
         }
-        int basketUsed = 0;
-        for(int i=0; i<n; i++){
-            if(basketFill[i]==1){
-                basketUsed++;
-            }
-        }
-        // SegmentTree st = new SegmentTree(arr);
+        SegmentTree st = new SegmentTree(arr);
 
-        // int basketUsed = st.query(1, 0, n - 1, 0, n - 1);
+        int basketUsed = st.query(1, 0, n - 1, 0, n - 1);
 
         return n - basketUsed;
     }
