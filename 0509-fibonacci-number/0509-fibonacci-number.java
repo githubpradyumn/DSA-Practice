982b476d1956cd73e948931347a865187c0bb5d6
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-03-30 12:07:46 +0530

    Time: 0 ms (100%), Space: 41.5 MB (98.63%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    int[] dp;
+    public int fib(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solution(n);
+    }
+    public int solution(int n){
+        if(n<2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = solution(n-1)+solution(n-2);
+    }
+}
