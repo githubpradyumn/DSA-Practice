bfbe98d0b6f3134208b6a4dd09c4d11c940a8c5f
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-04-09 01:07:13 +0530

    Time: 1 ms (99.82%), Space: 50.5 MB (27.89%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    int[][] dp;
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        dp = new int[m][n];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solve(m - 1, n - 1, grid);
+    }
+
+    public int solve(int i, int j, int[][] grid) {
+
+        if(i < 0 || j < 0) return Integer.MAX_VALUE;  //Integer.MAX_VALUE is for the out for boundary condition 
+
+        if(i == 0 && j == 0) return grid[0][0];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = solve(i - 1, j, grid);
+        int left = solve(i, j - 1, grid);
+
+        return dp[i][j] = grid[i][j] + Math.min(up, left);
+    }
+}
