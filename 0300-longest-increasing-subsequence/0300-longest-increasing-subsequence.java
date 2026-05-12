6b7486f8afd62c8c6bfcacba801061070c475268
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-05-12 14:42:35 +0530

    Time: 88 ms (19.62%), Space: 237.3 MB (9.27%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,39 @@
 class Solution {
-    public int lengthOfLIS(int[] nums){
+    public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n + 1][n + 1];
+        
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
+                int length = dp[idx + 1][prevIdx + 1];
+                if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+                    length = Math.max(length, 1 + dp[idx + 1][idx + 1]);
+                }
+                
+                dp[idx][prevIdx + 1] = length;
+            }
         }
-        return helper(nums,0,-1,dp);
+        
+        return dp[0][0];
     }
-    public int helper(int[] nums, int idx, int prevIdx, int[][] dp){
-        if(idx==nums.length) return 0;
-        if(dp[idx][prevIdx+1]!=-1) return dp[idx][prevIdx+1];
-        int notTake = helper(nums,idx+1,prevIdx,dp);
-        int take = 0;
-        if(prevIdx == -1 || nums[idx]>nums[prevIdx]){
-            take = 1 + helper(nums,idx+1,idx,dp);
-        }
-        return dp[idx][prevIdx+1] = Math.max(take,notTake);
-    }
+    // public int lengthOfLIS(int[] nums){
+    //     int n = nums.length;
+    //     int[][] dp = new int[n+1][n+1];
+    //     for(int[] row: dp){
+    //         Arrays.fill(row,-1);
+    //     }
+    //     return helper(nums,0,-1,dp);
+    // }
+    // public int helper(int[] nums, int idx, int prevIdx, int[][] dp){
+    //     if(idx==nums.length) return 0;
+    //     if(dp[idx][prevIdx+1]!=-1) return dp[idx][prevIdx+1];
+    //     int notTake = helper(nums,idx+1,prevIdx,dp);
+    //     int take = 0;
+    //     if(prevIdx == -1 || nums[idx]>nums[prevIdx]){
+    //         take = 1 + helper(nums,idx+1,idx,dp);
+    //     }
+    //     return dp[idx][prevIdx+1] = Math.max(take,notTake);
+    // }
     // public int lengthOfLIS(int[] nums) {
     //     return helper(nums,0,-1);
     // }
