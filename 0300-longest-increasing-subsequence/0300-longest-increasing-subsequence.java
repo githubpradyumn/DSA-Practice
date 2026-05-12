b34dbc21d1a184e0ddb71493ffeee059d40d6248
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-05-12 15:14:15 +0530

    Time: 40 ms (51.81%), Space: 45.9 MB (82.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,38 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] nums){
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
-                int length = dp[idx + 1][prevIdx + 1];
-                if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-                    length = Math.max(length, 1 + dp[idx + 1][idx + 1]);
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1; i<n; i++){
+            for(int j=i-1; j>=0; j--){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],1+dp[j]);
                 }
-                
-                dp[idx][prevIdx + 1] = length;
             }
         }
-        
-        return dp[0][0];
+        int max = 0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,dp[i]);
+        }
+        return max;
     }
+    // public int lengthOfLIS(int[] nums) {
+    //     int n = nums.length;
+    //     int[][] dp = new int[n + 1][n + 1];
+        
+    //     for (int idx = n - 1; idx >= 0; idx--) {
+    //         for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
+    //             int length = dp[idx + 1][prevIdx + 1];
+    //             if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+    //                 length = Math.max(length, 1 + dp[idx + 1][idx + 1]);
+    //             }
+                
+    //             dp[idx][prevIdx + 1] = length;
+    //         }
+    //     }
+        
+    //     return dp[0][0];
+    // }
     // public int lengthOfLIS(int[] nums){
     //     int n = nums.length;
     //     int[][] dp = new int[n+1][n+1];
