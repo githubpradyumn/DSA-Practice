bc6c29b5a9e69fc7b75883080054bfbbe84687e3
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-05-14 16:59:27 +0530

    Time: 7 ms (80.11%), Space: 46.1 MB (54.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        if (nums == null || nums.length == 0) return 0;
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < result.get(0)) {
+                result.set(0, nums[i]);
+            } 
+            else if (nums[i] > result.get(result.size() - 1)) {
+                result.add(nums[i]);
+            } 
+            else {
+                int start = 0;
+                int end = result.size() - 1;
+                while (start < end) {
+                    int mid = start + (end - start) / 2;
+                    if (result.get(mid) < nums[i]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid;
+                    }
+                }
+                result.set(start, nums[i]);
+            }
         }
-        return helper(nums,0,-1,dp);
-    }
-    public int helper(int[] nums, int idx, int preidx,int[][] dp){
-        if(idx==nums.length) return 0;
-        if(dp[idx][preidx+1]!=-1) return dp[idx][preidx+1];
-        int notTake = helper(nums,idx+1,preidx,dp);
-        int take = 0;
-        if(preidx==-1 || nums[idx]>nums[preidx]){
-            take = 1 + helper(nums,idx+1,idx,dp);
-        }
-        return dp[idx][preidx+1] = Math.max(take,notTake);
+        return result.size();
     }
 }
