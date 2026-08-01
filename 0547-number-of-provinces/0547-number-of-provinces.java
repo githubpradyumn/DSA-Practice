c0712a024319811f1ef17c9fed72a6743a0da7ce
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-08-02 00:37:55 +0530

    Time: 3 ms (26.23%), Space: 49.4 MB (12.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,39 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int V = isConnected.length;
-        int[] vis = new int[V];
+        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adjlist.add(new ArrayList<>());
+        }
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(isConnected[i][j]==1){
+                    adjlist.get(i).add(j);
+                }
+            }
+        }
+        int[] visited = new int[V];
         int count = 0;
-        for (int i = 0; i < V; i++) {
-            if (vis[i] == 0) {
-                vis[i] = 0;
-                dfs(isConnected, V, vis, i);
+        for(int i=0;i<V;i++){
+            if(visited[i]==0){
+                bfs(adjlist,visited,i);
                 count++;
             }
         }
         return count;
     }
-
-    public void dfs(int[][] isConnected, int V, int[] vis,int start){
-        vis[start]=1;
-        for(int i = 0;i<V;i++){
-            if((isConnected[start][i]==1) && (vis[i]==0)){
-                dfs(isConnected,V,vis,i);
+    public void bfs(ArrayList<ArrayList<Integer>> adjlist ,int[] visited, int node){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        visited[node] = 1;
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            for(int nbh:adjlist.get(curr)){
+                if(visited[nbh]!=1){
+                    visited[nbh]=1;
+                    q.add(nbh);
+                }
             }
         }
-        
-    }   
+    }
 }
