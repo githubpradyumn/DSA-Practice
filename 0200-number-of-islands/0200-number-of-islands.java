6f390347505367c1d5f5cafe10324807c21d8a52
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-08-02 21:52:27 +0530

    Time: 6 ms (20.19%), Space: 51.4 MB (89.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,45 +1,47 @@
 class Solution {
-    class Pair {
-        int row, col;
-        Pair(int row, int col){
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
             this.row=row;
             this.col=col;
         }
     }
-    int[] rowDirections = {0,0,-1,1};
-    int[] colDirections = {-1,1,0,0};
-    public boolean isValid(int row, int col, int totalRows, int totalCols){
-        return (row>=0 && col>=0 && row<totalRows && col<totalCols);
+    int[] Row = {-1,1,0,0};
+    int[] Col = {0,0,-1,1};
+    public boolean isValid(int row, int col, int m, int n){
+        return (row>=0 && col>=0 && row<m && col<n);
     }
     public int numIslands(char[][] grid) {
-        int totalRows = grid.length;
-        int totalCols = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         int count = 0;
-        int[][] isVisited = new int[totalRows][totalCols];
-        for(int i=0;i<totalRows;i++){
-            for(int j=0;j<totalCols;j++){
-                if(grid[i][j]=='1' && isVisited[i][j]==0){
-                    bfs(grid,isVisited,i,j,totalRows,totalCols);
+        int[][] isVisited = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(isVisited[i][j]==0 && grid[i][j]=='1'){
+                    bfs(grid,isVisited,m,n,i,j);
                     count++;
                 }
             }
         }
-    return count;
+        return count;
     }
-    public void bfs(char[][] grid, int[][] isVisited, int row, int col, int totalRows, int totalCols){
+    public void bfs(char[][] grid,int[][] isVisited, int m, int n, int row, int col){
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(row,col));
-        // isVisited[row][col]=1;
+        q.add(new Pair(row, col));
+        isVisited[row][col] = 1;
         while(!q.isEmpty()){
             Pair curr = q.poll();
             for(int i=0;i<4;i++){
-                int currRow = curr.row + rowDirections[i];
-                int currCol = curr.col + colDirections[i];
-                if(isValid(currRow,currCol,totalRows,totalCols) && grid[currRow][currCol]=='1' && isVisited[currRow][currCol]==0){
-                    isVisited[currRow][currCol]=1;
-                    q.add(new Pair(currRow,currCol));
+                int currRow = curr.row + Row[i];
+                int currCol = curr.col + Col[i];
+                if(isValid(currRow,currCol,m,n)&&grid[currRow][currCol]=='1'&&isVisited[currRow][currCol]==0){
+                    q.add(new Pair(currRow, currCol));
+                    isVisited[currRow][currCol] = 1;
                 }
             }
         }
     }
+
 }
