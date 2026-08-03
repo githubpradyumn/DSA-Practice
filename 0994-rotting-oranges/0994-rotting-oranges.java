5a7bab676b32d5effa8deba7b67430a3901348c3
Pradyumn Pratap Singh <166645447+githubpradyumn@users.noreply.github.com>
2026-08-03 22:34:30 +0530

    Time: 2 ms (89.88%), Space: 44.5 MB (13.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,67 +1,79 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    int dr[] = {0,0,-1,1};
+    int dc[] = {-1,1,0,0};
+    
+    public boolean isValid(int row, int col, int totalRow, int totalCol){
+        return (row>=0 && col>=0 && totalRow>row && totalCol>col); 
+    }
+
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        
+        int m = grid.length;
+        
+        int n = grid[0].length;
+
+        int freshOrange = 0;
         int time = 0;
-        int freshOranges = 0;
-        boolean freshFound = false;
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    freshOranges++;
-                } else if (grid[i][j] == 2) {
-                    q.add(new int[] { i, j });
+        boolean fresh = false;
+
+        Queue<Pair> q = new LinkedList<>();
+        int[][] isVisited = new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2 && isVisited[i][j]==0){
+                    q.add(new Pair(i,j));
+                    isVisited[i][j]=1;
+                } else if (grid[i][j]==1){
+                    freshOrange++;
                 }
             }
         }
-        while (!q.isEmpty() && freshOranges != 0) {
-            int qSize = q.size();
+        
+        while(!q.isEmpty()){
 
-            while (qSize > 0) {
-                int[] currOranges = q.remove();
-                int x = currOranges[0];
-                int y = currOranges[1];
-                //left
-                if ((y - 1) >= 0 && grid[x][y - 1] == 1) {
-                    freshOranges--;
-                    grid[x][y - 1] = 2;
-                    freshFound = true;
-                    q.add(new int[] { x, y - 1 });
-                }
-                // right
-                if ((y + 1 < m) && grid[x][y + 1] == 1) {
-                    freshOranges--;
-                    grid[x][y + 1] = 2;
-                    freshFound = true;
-                    q.add(new int[] { x, y + 1 });
-                }
-                // top
-                if ((x - 1) >= 0 && grid[x - 1][y] == 1) {
-                    freshOranges--;
-                    grid[x - 1][y] = 2;
-                    freshFound = true;
-                    q.add(new int[] { x - 1, y });
-                }
-                // bottom
-                if ((x + 1) < n && grid[x + 1][y] == 1) {
-                    freshOranges--;
-                    grid[x + 1][y] = 2;
-                    freshFound = true;
-                    q.add(new int[] { x + 1, y });
-                }
+            int size = q.size();
+            
+            while(size>0){
+            
+                Pair curr = q.poll();
+                
+                for(int i=0;i<4;i++){
 
-                qSize--;
+                    int currRow = curr.row + dr[i];
+                    int currCol = curr.col + dc[i];
+
+                    if(isValid(currRow,currCol,m,n) && isVisited[currRow][currCol]==0 && grid[currRow][currCol]==1){
+                        q.add(new Pair(currRow, currCol));
+                        isVisited[currRow][currCol]=1;
+                        freshOrange--;
+                        fresh = true;
+                    }
+                }
+                size--;
             }
-            if (freshFound == true) {
+
+            if(fresh){
                 time++;
+                fresh = false;
             }
+
         }
 
-        if (freshOranges == 0) {
-            return time;
-        } else {
+        if(freshOrange!=0){
             return -1;
         }
+
+
+        return time;
     }
 }
